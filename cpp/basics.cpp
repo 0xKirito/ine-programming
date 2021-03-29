784c27be3d705efebf763209355130c22ee8145c
0xKirito <40525297+0xKirito@users.noreply.github.com>
2021-03-29 08:33:55 +0530

    init: ine cpp

---
+++ cpp/basics.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello World!";
+  // both the lines below will prevent the cli for closing automatically
+  // system("PAUSE");
+  cin.ignore();
+  return 0;
+}
